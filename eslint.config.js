2209b3252ca0307d47854581392e923d34d4e942
Olumide Bakare <olumide.bakare@outsourceglobal.com>
2025-11-26 19:10:49 +0100

    chore: update eslint config and generated fixes

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,7 @@ export default [
     files: ['src/generated/**'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
     },
   },
 ];
